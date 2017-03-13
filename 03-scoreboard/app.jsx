6e6b8c3ef4d446188c1243c4482d5036a556ab3d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-03-12 21:12:50 -0500

    Added mockup of application

--- 03-scoreboard/app.jsx
+++ 03-scoreboard/app.jsx
@@ -1,8 +1,38 @@
 function Application() {
+  //  All react components must be contained in a single element
+  //  React components in JSX look like HTML, but are not.  It is compiled into JavaScript
+  //  Use className instead of class, as class is reserved for OOP in JavaScript
   return (
-    <div>
-      <h1>Hello from React</h1>
-      <p>I was rendered from the Application component!</p>
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Barrett Amsrud
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 15 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+        <div className="player">
+          <div className="player-name">
+            Andrew Carlson
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 10 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
