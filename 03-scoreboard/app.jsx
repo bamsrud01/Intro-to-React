de4b7b8c68442e9c22a5e15e6994ab49ce6e9c72
Barrett Amsrud <barrettamsrud@gmail.com>
2017-03-24 23:56:36 -0500

    Added dynamic buttons to Stopwatch component

--- 03-scoreboard/app.jsx
+++ 03-scoreboard/app.jsx
@@ -19,13 +19,29 @@ var PLAYERS = [
 var nextID = 4;
 
 var Stopwatch = React.createClass({
+  getInitialState: function() {
+    return {
+      running: false,
+    }
+  },
+  onStart: function() {
+    this.setState({ running: true });
+  },
+  onStop: function() {
+    this.setState({ running: false });
+  },
+  onReset: function() {},
   render: function() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <div className="stopwatch-time">0</div>
-        <button>Start</button>
-        <button>Reset</button>
+        { this.state.running ?
+          <button onClick={this.onStop}>Stop</button>
+          :
+          <button onClick={this.onStart}>Start</button>
+        }
+        <button onClick={this.onReset}>Reset</button>
       </div>
     );
   },
